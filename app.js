a31a2cef9aa21a5fcfff72dadbdcdbb8b53d50eb
patgrantart <patgrantart@gmail.com>
2024-05-30 06:06:32 +1000

    environment port bug fixed

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 console.log("node app running")
 
 const express = require('express');
-
 const app = express();
 
-app.listen(3000, ()=> console.log(`listening at 3000`));
 
+require("process");
+const port = process.env.PORT || 3000;
+console.log(process);
+console.log(process.env.PORT)
+const homeDirectory = process.cwd();
+
+console.log(homeDirectory);
+
+app.listen(port, ()=> console.log(`server listening`));
 app.use(express.static('public'));
-//serve web page
-//recieve update
 
 
 console.log('node script complete')
